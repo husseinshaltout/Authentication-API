679406d252f877e834e3b4d428fb68ea82f7ca25
Hussein Shaltout <48390004+husseinshaltout@users.noreply.github.com>
2023-12-10 23:18:07 +0200

    test(auth): Add incomplete data checks in signin & signup endpoint tests

--- src/app/auth/__tests__/auth.controller.test.ts
+++ src/app/auth/__tests__/auth.controller.test.ts
@@ -25,6 +25,66 @@ describe('AuthController', () => {
         `Created User ${userData.firstName} ${userData.lastName} Successfully`
       );
     });
+
+    describe('POST /signup - Bad Scenarios', () => {
+      it('should return validation error for incomplete data', async () => {
+        const incompleteUserData = {
+          email: 'incomplete@example.com',
+          password: 'password123',
+        };
+
+        const response = await request(app)
+          .post('/api/auth/signup')
+          .send(incompleteUserData)
+          .expect(HttpStatus.BAD_REQUEST);
+
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            error: 'Bad Request',
+            message: 'Validation failed',
+            statusCode: 400,
+            validation: {
+              body: {
+                keys: ['passwordConfirm'],
+                message: '"passwordConfirm" is required',
+                source: 'body',
+              },
+            },
+          })
+        );
+      });
+
+      it('should return validation error for invalid data', async () => {
+        const invalidUserData = {
+          firstName: 'Invalid*Name',
+          lastName: '123',
+          email: 'invalidemail',
+          password: 'pass',
+          passwordConfirm: 'password',
+        };
+
+        const response = await request(app)
+          .post('/api/auth/signup')
+          .send(invalidUserData)
+          .expect(HttpStatus.BAD_REQUEST);
+
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            error: 'Bad Request',
+            message: 'Validation failed',
+            statusCode: 400,
+            validation: {
+              body: {
+                keys: ['firstName'],
+                message:
+                  '"firstName" must only contain alpha-numeric characters',
+                source: 'body',
+              },
+            },
+          })
+        );
+      });
+    });
   });
 
   describe('POST /signin', () => {
@@ -44,6 +104,54 @@ describe('AuthController', () => {
       expect(response.body.lastName).toBeDefined();
       expect(response.body.email).toBe(loginData.email);
       expect(response.body.accessToken).toBeDefined();
+    });
+    describe('POST /signin - Bad Scenarios', () => {
+      it('should return validation error for incomplete data', async () => {
+        const incompleteLoginData = {
+          password: 'password123',
+        };
+
+        const response = await request(app)
+          .post('/api/auth/signin')
+          .send(incompleteLoginData)
+          .expect(HttpStatus.BAD_REQUEST);
+
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            error: 'Bad Request',
+            message: 'Validation failed',
+            statusCode: 400,
+            validation: {
+              body: {
+                keys: ['email'],
+                message: '"email" is required',
+                source: 'body',
+              },
+            },
+          })
+        );
+      });
+
+      it('should return unauthorized for incorrect credentials', async () => {
+        const incorrectLoginData = {
+          email: 'test@example.com',
+          password: 'incorrectpassword',
+        };
+
+        const response = await request(app)
+          .post('/api/auth/signin')
+          .send(incorrectLoginData)
+          .expect(HttpStatus.UNAUTHORIZED);
+
+        expect(response.body).toEqual({
+          errors: [
+            {
+              message: 'Wrong Email or Password! Please Try Again',
+            },
+          ],
+          message: 'Wrong Email or Password! Please Try Again',
+        });
+      });
     });
   });
 });
