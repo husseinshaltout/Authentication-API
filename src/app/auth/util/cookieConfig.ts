a03ab4658310b7741294581221560da93d247dbb
Hussein Shaltout <48390004+husseinshaltout@users.noreply.github.com>
2023-12-10 20:21:57 +0200

    chore(cookie): Update secure flag based on NODE_ENV

--- src/app/auth/util/cookieConfig.ts
+++ src/app/auth/util/cookieConfig.ts
@@ -2,7 +2,7 @@ import config from '@config';
 
 const defaultCookieOptions = {
   httpOnly: true,
-  secure: false,
+  secure: config.NODE_ENV === 'production' ? true : false,
   sameSite: config.NODE_ENV === 'production' ? true : 'none',
 } as const;
 
